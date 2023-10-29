75f3097a21425097b2471744e4015c5b6772f91e
Jaid <6216144+Jaid@users.noreply.github.com>
2023-10-29 17:30:22 +0100

    Improved healthCheck and fixed workflow
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/healthCheck.bash
+++ src/healthCheck.bash
@@ -1,13 +1,24 @@
 #!/bin/bash
 set -o errexit -o pipefail
 
-ports --mine
+if [[ -n $debug ]]; then
+  ports --mine
+fi
 pid=$(pgrep --uid "$userId" ^ssh)
+if [[ -n $debug ]]; then
+  printf 'pid = %s\n' "$pid"
+fi
 ramUsed=$(ps --pid "$pid" -o vsz h)
+if [[ -n $debug ]]; then
+  printf 'ramUsed = %s\n' "$ramUsed"
+fi
 if [[ $ramUsed -lt 100 ]]; then
   exit 1
 fi
 netLine=$(netstat --wide --tcp | grep --extended-regexp ':22\s+ESTABLISHED$')
+if [[ -n $debug ]]; then
+  printf 'netLine = %s\n' "$netLine"
+fi
 if [[ -z $netLine ]]; then
   exit 1
 fi
